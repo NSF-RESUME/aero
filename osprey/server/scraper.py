a9b7a4516711c69c52df97c175bfd7d64aa59a3c
ValHayot <valeriehayot@gmail.com>
2023-05-24 21:28:59 +0000

    Add additional metadata to scraper logic

--- osprey/server/scraper.py
+++ osprey/server/scraper.py
@@ -1,11 +1,23 @@
 """Database scraper implementation."""
 import json
+import hashlib
 import os
 import requests
 
+from typing import NamedTuple
+
 from osprey.server.proxies import proxify
 
-_avail_data: dict[str, tuple[str, str]] = {}
+
+class Metadata(NamedTuple):
+    name: str
+    url: str
+    checksum: str
+    last_modif: str
+    version: int
+
+
+_avail_data: dict[str, list[Metadata]] = {}
 
 
 # TODO: store in actual databases
@@ -16,14 +28,31 @@ def available_databases() -> None:
         if fn not in _avail_data:
             with open(os.path.join(path, fn), "r") as f:
                 metadata = json.load(f)
-            _avail_data[fn] = (metadata["name"], metadata["url"])
+            _avail_data[fn] = [
+                Metadata(metadata["name"], metadata["url"], None, None, 0)
+            ]
 
 
 def scrape_database() -> None:
     """Provided list of all databases, scrape and proxy data."""
-    for name, url in _avail_data.values():
-        req = requests.get(url)
-        proxify(name, req.json())
+    for key, v in _avail_data.items():
+        m = v[-1]  # Get last update in list of files
+        req = requests.get(m.url)
+        data = req.json()
+        new_hash = hashlib.md5(json.dumps(data).encode("utf-8")).hexdigest()
+        last_modif = req.headers["Last-Modified"]
+
+        if new_hash == m.checksum:
+            return
+
+        proxify(m.name, data)
+
+        if m.checksum is None:
+            _avail_data[key] = [Metadata(m.name, m.url, new_hash, last_modif, 1)]
+        else:
+            _avail_data[key].append(
+                Metadata(m.name, m.url, new_hash, last_modif, m["version"] + 1)
+            )
 
 
 available_databases()
