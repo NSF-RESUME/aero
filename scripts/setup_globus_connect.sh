69be929d8e5cf0528b7fac3323a0a892ace817ed
Sudershan Sridhar <sudershan@SudershanMacBookAir.local>
2023-08-14 15:31:23 -0700

    Adding Docker file for gsc node

---
+++ scripts/setup_globus_connect.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+globus-connect-server endpoint setup "My GCSv5.4 Endpoint" \
+    --organization “OspreyDev” \
+    --owner sudershan@uchicago.edu \
+    --contact-email sudershan@uchicago.edu \
+    --project-id ff6bae42-f7a6-4833-ac92-792e5b9a69fa
