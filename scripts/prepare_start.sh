b23824a87e1a56e407cecdfa43746c1470e278c5
Sudershan Sridhar <sudershan@SudershanMacBookAir.local>
2023-09-01 15:22:44 -0500

    Verifier Modifier flow done

--- scripts/prepare_start.sh
+++ scripts/prepare_start.sh
@@ -23,6 +23,7 @@ out=`docker compose run -it globus-endpoint python /app/osprey/worker/lib/globus
 
 flow_download_uuid=`echo ${out} | grep -i 'download' | awk '{print $NF}'`
 flow_database_uuid=`echo ${out} | grep -i 'database' | awk '{print $NF}'`
+flow_user_function_uuid=`echo ${out} | grep -i 'user' | awk '{print $NF}'`
 
 echo "${out}"
 
@@ -30,7 +31,7 @@ echo "\n\nRunning migrations"
 docker compose up postgres-database -d
 docker compose exec -it postgres-database bash -c "psql -U postgres -c \"CREATE DATABASE osprey_development;\"; exit;"
 docker compose run -it web flask db upgrade
-docker compose down postgres-database
+docker compose down
 
 echo "\n\nSetting up Globus Flow Worker"
 docker compose run -it web python /app/osprey/server/jobs/timer.py
@@ -39,5 +40,6 @@ docker compose run -it web python /app/osprey/server/lib/globus_compute.py
 sed -i '' "s/GLOBUS_WORKER_UUID=.*/GLOBUS_WORKER_UUID=${endpoint_uuid}/g" docker-compose.yml
 sed -i '' "s/GLOBUS_FLOW_DOWNLOAD_FUNCTION=.*/GLOBUS_FLOW_DOWNLOAD_FUNCTION=${flow_download_uuid}/g" docker-compose.yml
 sed -i '' "s/GLOBUS_FLOW_COMMIT_FUNCTION=.*/GLOBUS_FLOW_COMMIT_FUNCTION=${flow_database_uuid}/g" docker-compose.yml
+sed -i '' "s/GLOBUS_FLOW_USER_COMMIT_FUNCTION=.*/GLOBUS_FLOW_USER_COMMIT_FUNCTION=${flow_user_function_uuid}/g" docker-compose.yml
 
 echo "\n\nUpdated docker-compose.yml"
