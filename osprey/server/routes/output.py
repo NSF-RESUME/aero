f1bed5ade11d412e18130189ec2943de8a3f53a7
valhayot <valeriehayot@gmail.com>
2023-11-30 20:12:11 -0600

    [WIP] Add route for outputs

---
+++ osprey/server/routes/output.py
@@ -0,0 +1,32 @@
+import uuid
+
+from pathlib import Path
+
+from flask import Blueprint, jsonify, request
+from osprey.server.app import db
+from osprey.server.app.models import Output
+from osprey.server.lib.error import ServiceError
+
+output_routes = Blueprint('output_routes', __name__, url_prefix='/output')
+GCS_DIR = Path('/dsaas_storage/output') 
+# TODO: Just create these directories when GCS is created
+GCS_DIR.mkdir(parents=True, exist_ok=True)
+
+@output_routes.route('/', methods=['GET'])
+def show_output():
+    page        = request.args.get('page') or 1
+    per_page    = request.args.get('per_page') or 15
+    outputs     = Output.query.paginate(page=page, per_page=per_page)
+    result      = [o.toJSON() for o in outputs]
+    return jsonify(result), 200
+
+@output_routes.route('/new', methods=['POST'])
+def create_output_file():
+    filename = str(uuid.uuid4())
+    filepath = Path(GCS_DIR, filename)
+    filepath.touch()
+
+    return jsonify({ 'file': str(filepath) }), 200
+
+    
+    
