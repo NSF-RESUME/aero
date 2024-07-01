95179ae618fbd5143986b7f149f67034e23ed574
valhayot <valeriehayot@gmail.com>
2024-07-01 12:50:55 -0500

    Add test configuration file

--- osprey/tests/conftest.py
+++ osprey/tests/conftest.py
@@ -1,50 +1,26 @@
-import os
+import pytest
 
-os.environ["DATABASE_HOST"] = "127.0.0.1"
-os.environ["DATABASE_USER"] = "postgres"
-os.environ["DATABASE_PASSWORD"] = "postgres"
-os.environ["DATABASE_PORT"] = "5001"
-os.environ["DATABASE_NAME"] = "osprey_development"
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
+from unittest import mock
 
-# #from osprey.server.app import routes
+with (
+    mock.patch("osprey.server.lib.globus_search.DSaaSSearchClient") as dsc_mock,
+    mock.patch("osprey.server.jobs.timer.set_timer", return_value=1111) as st_mock,
+    mock.patch("osprey.server.jobs.user_flow.run_flow") as rf_mock,
+):
+    from osprey.server.app import create_app, db
 
-# import pytest
 
-# #TODO: replace app __init__.py with a `create_app` function and remove boilerplate code
-# @pytest.fixture()
-# def app():
-#     DATABASE_PORT=5432
-#     DATABASE_PASSWORD='postgres'
-#     DATABASE_USER='postgres'
-#     DATABASE_HOST='127.0.0.1' #'osprey-postgres-database-1'
-#     DATABASE_NAME='osprey_development'
+@pytest.fixture(scope="session")
+def app():
+    app = create_app()
+    app.config.update({"TESTING": True})
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.session.remove()
+        db.drop_all()
 
-#     config = {
-#         'DATABASE_HOST': DATABASE_HOST,
-#         'DATABASE_USER': DATABASE_USER,
-#         'DATABASE_PASSWORD': DATABASE_PASSWORD,
-#         'PORT': DATABASE_PORT,
-#         'DATABASE_NAME': DATABASE_NAME,
-#         'SQLALCHEMY_DATABASE_URI' : f'postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
-#     }
-#     from osprey.server.app import app
-#     # db = SQLAlchemy()
-#     # migrate = Migrate()
-#     # app = Flask(__name__)
 
-#     # app.config.from_mapping(config)
-
-#     # db.init_app(app)
-#     # migrate.init_app(app, db)
-
-#     # app.register_blueprint(routes.all_routes)
-#     app.config.update(config)
-
-#     yield app
-
-# @pytest.fixture
-# def client(app):
-#     return app.test_client()
+@pytest.fixture()
+def client(app):
+    return app.test_client()
