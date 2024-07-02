40a47273e134bd98567cd76e8e7c7e871f934d37
valhayot <valeriehayot@gmail.com>
2024-07-02 15:30:10 -0500

    Remove unused code and add error handling test cases

---
+++ tests/app/error_test.py
@@ -0,0 +1,30 @@
+from osprey.server.app import error_handler
+
+
+def test_forbidden():
+    eh = error_handler.ForbiddenError()
+
+    assert eh.message is not None and eh.status_code == 403 and eh.payload is None
+
+    ret = eh.to_dict()
+    assert list(ret.keys()) == ["message"]
+    assert ret["message"] == "Permission Denied"
+
+    message = "test"
+    status_code = 100
+    eh = error_handler.ForbiddenError(message=message, status_code=status_code)
+    assert eh.message == message and eh.status_code == status_code
+
+
+def test_unauthorized():
+    eh = error_handler.UnauthorizedError()
+    assert eh.message is not None and eh.status_code == 401 and eh.payload is None
+
+    ret = eh.to_dict()
+    assert list(ret.keys()) == ["message"]
+    assert ret["message"] == "Not Authorized"
+
+    message = "test"
+    status_code = 100
+    eh = error_handler.UnauthorizedError(message=message, status_code=status_code)
+    assert eh.message == message and eh.status_code == status_code
